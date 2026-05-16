a8b5a3012b253619ef0f8d4f67bcb18b595b1fde
Nosliejnav <vanjeilsonm10@gmail.com>
2026-05-16 18:23:26 -0300

    Utilizando Refresh Tokens para renovar sessão

--- src/main/java/com/vanja/libraryapi/config/new.sh
+++ src/main/java/com/vanja/libraryapi/config/new.sh
@@ -1,5 +1,5 @@
-git add .
-git commit -m "Se autenticando com Authorization Code grant type"
+#git add .
+#git commit -m "Utilizando Refresh Tokens para renovar sessão"
 git checkout main
 git merge devel
 git push origin main
