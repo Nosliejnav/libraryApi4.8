3ade86e5e90ecac69ba8f0b1202a915a4e23f37e
Nosliejnav <vanjeilsonm10@gmail.com>
2026-05-16 17:51:31 -0300

    Se autenticando com Authorization Code grant type

--- src/main/java/com/vanja/libraryapi/controller/LoginVIewController.java
+++ src/main/java/com/vanja/libraryapi/controller/LoginVIewController.java
@@ -4,6 +4,7 @@ import com.vanja.libraryapi.security.CustomAuthentication;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -21,5 +22,11 @@ public class LoginVIewController {
             System.out.println(customAuth.getUsuario());
         }
         return "Olá " + authentication.getName();
+    }
+
+    @GetMapping("/authorized")
+    @ResponseBody
+    public String getAuthorizationCode(@RequestParam("code") String code){
+        return "Seu authorization code: " + code;
     }
 }
